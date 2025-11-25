ec2d30957f1387e91cd6dab34e74f7aebabd5336
Fatima <blalfatmt32@gmail.com>
2025-11-26 02:26:30 +0300

    create frist fail

---
+++ calculator111/calculator.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter a first number: "))
+num2 = int(input("Enter a second number: "))
+
+operator = input("Enter a operator: +, -, *, /: ")
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "_":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
